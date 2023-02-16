f2a67442c5fb94b267eea0a84651734a6293a108
xmd <2191968433@qq.com>
2023-02-16 11:23:30 +0800

    style: 文件重命名

---
+++ 3-src-组件通讯-插槽/Child.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- 只有一个默认插槽出口，所有默认插槽的内容都会显示在这里 -->
+    <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+export default {};
+</script>
+<style lang="less"></style>
---
+++ 3-src-组件通讯-插槽/Child3.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>作用域插槽</div>
+  <!-- 把当前组件的num数据传给父组件 -->
+  <slot :num="num" @say="say"></slot>
+</template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      num: 10,
+    };
+  },
+  methods: {
+    say() {
+      console.log('hello word');
+    },
+  },
+};
+</script>
