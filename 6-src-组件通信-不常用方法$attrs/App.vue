15d9ccf8a9b803c7d9053c79c1d9fcb943cf4404
xmd <2191968433@qq.com>
2023-02-16 16:28:06 +0800

    feat: 组件通信--provide提供和inject注入

---
+++ 6-src-组件通信-不常用方法$attrs/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>不常用的通信方式 ： $attrs</p>
+    <!-- 单独传 -->
+    <Child
+      :person="person"
+      :hobby="hobby"
+      :num="num"
+      @say="say"
+      @handleName="handleName"
+    >
+    </Child>
+    <hr />
+  </div>
+</template>
+<script lang="ts">
+import Child from './Child.vue';
+
+export default {
+  name: 'App',
+  components: { Child },
+  data() {
+    return {
+      person: {
+        name: '张三',
+        age: 30,
+        address: '深圳',
+      },
+      num: 1,
+      hobby: ['吃饭', '唱歌', '逛街'],
+    };
+  },
+  methods: {
+    say() {
+      console.log('你好');
+    },
+    handleName() {
+      console.log('handleName');
+    },
+  },
+};
+</script>
--- 7-src-组件通信-provide提供-inject注入/App.vue
+++ 7-src-组件通信-provide提供-inject注入/App.vue
@@ -9,6 +9,8 @@ export default {
   name: 'App',
   components: { Parent },
   // 提供数据. 只要是app的后代组件,都可以接收到这些数据
+  // 作用: 可以跨级通信,
+  // 应用场景 : 通常 祖孙之间 . 父子组件也可以,但是父子组件通常使用porps的方式
   provide: {
     person: {
       name: '张三',
