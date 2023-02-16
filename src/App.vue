9f665a333e96f64e06f5ae4ef82f1546171a8872
xmd <2191968433@qq.com>
2023-02-16 09:59:49 +0800

    feat: 组件通信-v-model绑定多个属性

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <!-- 默认 -->
     <Home v-model="age"></Home>
 
-    <!-- 绑定多个，起个名字 -->
+    <!-- 绑定多个，起个名字
+      v-model:xxx= 'yyy'
+      绑定了xxx属性
+      绑定了update:xxx自定义事件
+    -->
     <Home v-model:num="num"></Home>
+    <Home v-model:list="list"></Home>
   </div>
 </template>
 <script lang="ts">
@@ -16,7 +21,8 @@ export default {
   data() {
     return {
       age: 20,
-      num: 0,
+      num: 1,
+      list: [1, 2, 3, 4],
     };
   },
 };
--- src/home.vue
+++ src/home.vue
@@ -1,9 +1,11 @@
 <template>
   <h1>home</h1>
-  <p>{{ modelValue }}</p>
-  <p>{{ num }}</p>
+  <p v-if="modelValue">年龄： {{ modelValue }}</p>
+  <p v-if="num">num: {{ num }}</p>
+  <p v-if="list.length">{{ list }}</p>
   <button @click="$emit('update:modelValue', modelValue + 1)">age + 1</button>
   <button @click="$emit('update:num', num + 1)">num+1</button>
+  <button @click="changeList">修改list</button>
 </template>
 
 <script lang="ts">
@@ -16,11 +18,20 @@ export default {
     num: {
       type: Number,
     },
+    list: {
+      type: Array,
+      default: () => [],
+    },
   },
   // 声明接收事件
-  emits: ['update:modelValue', 'update:num'],
+  emits: ['update:modelValue', 'update:num', 'update:list'],
   mounted() {
     console.log(this);
+  },
+  methods: {
+    changeList() {
+      this.$emit('update:list', ['张三', 30]);
+    },
   },
 };
 </script>
