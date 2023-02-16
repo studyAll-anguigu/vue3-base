2557f0bc4b1afdea5756b84a500d0dec7cbf98ab
xmd <2191968433@qq.com>
2023-02-16 12:06:08 +0800

    feat: 组件通信--$attrs

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,42 @@
 <template>
   <div>
-    <p>app</p>
-    <Child>
-      <!-- 默认插槽 三个板块都会在child组件中作显示在 <slot></slot> -->
-      <div>标题</div>
-      <div>内容区</div>
-      <div>footer</div>
+    <p>不常用的通信方式 ： $attrs</p>
+    <!-- 单独传 -->
+    <Child
+      :person="person"
+      :hobby="hobby"
+      :num="num"
+      @say="say"
+      @handleName="handleName"
+    >
     </Child>
     <hr />
-
-    <Child2>
-      <!-- 具名插槽：
-        起名字需要在template标签上命名
-        推荐使用vue3提供指令 v-slot:slotName
-        当一个组件同时接收默认插槽和具名插槽时，所有位于顶级的非 <template> 节点都被隐式地视为默认插槽的内容。
-      -->
-      <template v-slot:header>
-        <div>header:标题</div>
-      </template>
-
-      <!-- 如果已存在template，且是默认插槽时，没有包含在改template的其他dom元素会被忽略 -->
-      <!-- <div>导航栏</div> -->
-      <template v-slot:default>
-        <div>defalut</div>
-        <div>你好</div>
-      </template>
-
-      <!-- 默认插槽，会显示在默认插槽出口 #是v-slot指令的简写 -->
-      <template #main>
-        <div>main:内容区</div>
-      </template>
-      <template v-slot:footer>
-        <div>footer</div>
-      </template>
-    </Child2>
-    <hr />
-
-    <!-- 作用域插槽 ： 父组件需要获取子组件的数据时，就需要用到作用域插槽 -->
-    <Child3 v-slot="data">
-      <!-- 接收整个对象 --->
-      <p>{{ data }}</p>
-      <p>{{ data.num }}</p>
-    </Child3>
-    <!-- 解构：只要我们需要的数据 -->
-    <Child3 v-slot="{ num }">{{ num }}</Child3>
-    <hr />
-
-    <!-- v-slot简写 -->
-    <Child4>
-      <!-- 完整写法 -->
-      <template v-slot:main="slotProps">
-        <p>
-          {{ slotProps }}
-        </p>
-      </template>
-    </Child4>
-    <Child4>
-      <!-- 简写 ,并解构需要的数据 -->
-      <template #main="{ list }">
-        <ul>
-          <li v-for="(item, index) in list" :key="index">{{ item }}</li>
-        </ul>
-      </template>
-    </Child4>
   </div>
 </template>
 <script lang="ts">
 import Child from './Child.vue';
-import Child2 from './Child2.vue';
-import Child3 from './Child3.vue';
-import Child4 from './Child4.vue';
 
 export default {
   name: 'App',
-  components: { Child, Child2, Child3, Child4 },
+  components: { Child },
   data() {
-    return {};
+    return {
+      person: {
+        name: '张三',
+        age: 30,
+        address: '深圳',
+      },
+      num: 1,
+      hobby: ['吃饭', '唱歌', '逛街'],
+    };
+  },
+  methods: {
+    say() {
+      console.log('你好');
+    },
+    handleName() {
+      console.log('handleName');
+    },
   },
 };
 </script>
--- src/Child.vue
+++ src/Child.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    <!-- 只有一个默认插槽出口，所有默认插槽的内容都会显示在这里 -->
-    <slot></slot>
-  </div>
+  <div>child</div>
+  <div>{{ $attrs }}</div>
+  <button @click="$emit('say')">say()</button>
 </template>
 <script lang="ts">
-export default {};
+export default {
+  name: 'XChild',
+  // 声明接收props
+  props: {
+    person: Object,
+  },
+  // 声明接收自定义事件
+  // 声明接收，就自动绑定为自定义事件
+  // 未声明接收，就绑定为dom事件
+  // 触发都是通过$emit触发自定义事件
+  emits: ['say'],
+  mounted() {
+    console.log(this);
+  },
+};
 </script>
 <style lang="less"></style>
+
+<!-- 
+  总结：
+  子组件接收父组件传过来的数据
+  - 在vue3中，所有 未声明的props 和 自定义事件 都存放在 this.$attrs中
+  - 只要在props中声明接收了，就不会存在$attrs中
+  - 同样，在emits中声明接收了，就不会存放在$attrs中了
+
+ -->
