cd57e4ec2df008858cd82760118e54bcee795c6c
xmd <2191968433@qq.com>
2023-02-20 15:03:31 +0800

    style: element-ui组件库的使用-完整引入方式

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,14 @@
-<!-- 
-  总结：
-  路由： 一级路由，嵌套路由
-  动态路由
-  路由参数： params，query，meta
-  路由缓存 : 
-    使用内置组件： transition 
-    按规范需要每个模板组件需要个根标签包裹，否则会有一个警告。但是不影响功能的使用
-  路由守卫 ：
-    beforeEach()
-    beforeResolve()
-    afterEach()
-    。。。
- -->
 <template>
-  <div>
-    <h1>App</h1>
-    <h1>路由跳转方式</h1>
-    <!-- 路由跳转方式1： 声明式跳转 -->
-    <router-link to="/home">Home</router-link><br />
-    <router-link :to="{ name: 'List' }">List</router-link><br />
-    <!-- 路由跳转方式2： 编程式跳转 -->
-    <a href="#" @click="toMeaage">去detail页面</a><br />
-
-    <h1>路由传参类型</h1>
-    <!-- 路由传参1： 查询字符串参数 query -->
-    <router-link :to="{ name: 'Home', query: { name: 'aaa' } }">
-      Home-query参数
-    </router-link>
-    <br />
-    <!-- 路由传参2： 动态路由参数 params -->
-    <router-link :to="{ name: 'Detail', params: { id: 66 } }">
-      detail-params参数
-    </router-link>
-
-    <!-- 一级路由组件出口 -->
-    <router-view v-slot="{ Component }">
-      <!-- 过度效果 -->
-      <transition name="fade">
-        <!-- 路由缓存,通过router-view暴露一个v-slot，这个主要是配合 路由缓存和路由过度使用 -->
-        <keep-alive>
-          <component :is="Component"></component>
-        </keep-alive>
-      </transition>
-    </router-view>
-  </div>
+  <div>App</div>
+  <!-- 使用 完整引入 组件库之后，其他组件无需再引入组件，即可直接使用 -->
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 
 <script lang="ts">
@@ -52,27 +17,6 @@ export default {
 };
 </script>
 
-<script lang="ts" setup>
-import { useRouter } from 'vue-router';
+<script lang="ts" setup></script>
 
-const router = useRouter();
-
-const toMeaage = () => {
-  router.push({ name: 'Detail', params: { id: 5 } });
-};
-</script>
-
-<style scoped>
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 1s;
-}
-.fade-enter-to,
-.fade-leave-from {
-  opacity: 1;
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router/index';
 
-createApp(App).use(router).mount('#app');
+// 完整引入 elemenui组件库
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+
+const app = createApp(App);
+
+app.use(ElementPlus);
+
+app.mount('#app');
