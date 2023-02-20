a2f6c5fc31d2797841fcd5ae1b25a6946d69bab4
xmd <2191968433@qq.com>
2023-02-20 15:15:51 +0800

    style: element-ui组件库的使用-按需引入方式

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,44 @@
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </el-row>
+
+  <el-row>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </el-row>
+
+  <div class="demo-datetime-picker">
+    <div class="block">
+      <span class="demonstration">Default</span>
+      <el-date-picker
+        v-model="value1"
+        type="datetime"
+        placeholder="Select date and time"
+      />
+    </div>
+    <div class="block">
+      <span class="demonstration">With shortcuts</span>
+      <el-date-picker
+        v-model="value2"
+        type="datetime"
+        placeholder="Select date and time"
+        :shortcuts="shortcuts"
+      />
+    </div>
+    <div class="block">
+      <span class="demonstration">With default time</span>
+      <el-date-picker
+        v-model="value3"
+        type="datetime"
+        placeholder="Select date and time"
+        :default-time="defaultTime"
+      />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -17,6 +55,66 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import {
+  Search,
+  Edit,
+  Check,
+  Message,
+  Star,
+  Delete,
+} from '@element-plus/icons-vue';
 
-<style scoped></style>
+const value1 = ref('');
+const value2 = ref('');
+const value3 = ref('');
+const defaultTime = new Date(2000, 1, 1, 12, 0, 0);
+
+const shortcuts = [
+  {
+    text: 'Today',
+    value: new Date(),
+  },
+  {
+    text: 'Yesterday',
+    value: () => {
+      const date = new Date();
+      date.setTime(date.getTime() - 3600 * 1000 * 24);
+      return date;
+    },
+  },
+  {
+    text: 'A week ago',
+    value: () => {
+      const date = new Date();
+      date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+      return date;
+    },
+  },
+];
+</script>
+
+<style scoped>
+.demo-datetime-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
+.demo-datetime-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+.demo-datetime-picker .block:last-child {
+  border-right: none;
+}
+.demo-datetime-picker .demonstration {
+  display: block;
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 // 完整引入 elemenui组件库
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+// import ElementPlus from 'element-plus';
+// import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
-app.use(ElementPlus);
+// app.use(ElementPlus);
 
 app.mount('#app');
